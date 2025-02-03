eefa40bc94d221ee4f5464a09ea455552115d411
wiselook <1656438881@qq.com>
2025-02-03 20:27:30 +0800

    Feat: add buff_controller and buff_motor_urdf.

---
+++ catkin_ws/src/buff_controller/include/buff_controller/buff_controller.h
@@ -0,0 +1,79 @@
+//
+// Created by wk on 2025/2/3.
+//
+#pragma once
+
+#ifndef SRC_BUFF_CONTROLLER_H
+#define SRC_BUFF_CONTROLLER_H
+
+// ROS
+#include <ros/ros.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/Float64.h>
+#include <controller_interface/controller.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <control_msgs/JointControllerState.h>
+#include <control_toolbox/pid.h>
+#include <memory>
+#include <realtime_tools/realtime_publisher.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include <boost/random.hpp>
+
+namespace buff_controller
+{
+class BuffController : public controller_interface::Controller<hardware_interface::EffortJointInterface>
+{
+public:
+  BuffController() = default;
+  ~BuffController() override = default;
+
+  bool init(hardware_interface::EffortJointInterface* effort_joint_interface, ros::NodeHandle& root_nh,
+            ros::NodeHandle& controller_nh) override;
+
+  void update(const ros::Time& time, const ros::Duration& period) override;
+
+  hardware_interface::JointHandle joint_;
+
+private:
+  void update_target_vel();
+  void update_joint_state();
+  void update_current_state(const std_msgs::Int32::ConstPtr& msg);
+
+  std::string joint_name_;
+  ros::Time start_time_;
+  ros::Time current_time_;
+  ros::Duration period_;
+
+  double target_vel_{};
+  double last_target_vel_{};
+  double current_position_{};
+  double current_velocity_{};
+  double current_effort_{};
+  double error_{};
+  double error_target_{};
+  double pid_out_{};
+  double Kf_{};
+  double feedForward_out_{};
+  double commanded_effort_{};
+  bool feedForward_{};
+  bool state_changed_{};
+
+  typedef enum
+  {
+    BIG = 1,
+    SMALL,
+  } BuffState;
+  int state_ = BIG;
+
+  ros::Subscriber state_sub_;
+  ros::Publisher target_vel_pub_;
+  control_toolbox::Pid joint_pid_controller_;
+
+}; /* buff_controller */
+
+}  // namespace buff_controller
+
+#endif  // SRC_BUFF_CONTROLLER_H
---
+++ catkin_ws/src/buff_controller/src/buff_controller.cpp
@@ -0,0 +1,113 @@
+//
+// Created by wk on 2025/2/3.
+//
+#include "buff_controller/buff_controller.h"
+#include <pluginlib/class_list_macros.hpp>
+
+namespace buff_controller
+{
+bool BuffController::init(hardware_interface::EffortJointInterface* effort_joint_interface, ros::NodeHandle& root_nh,
+                          ros::NodeHandle& controller_nh)
+{
+  if (!controller_nh.getParam("joint_name", joint_name_))
+  {
+    ROS_ERROR("Could not find joint_name parameter");
+    return false;
+  }
+  controller_nh.getParam("feedForward", feedForward_);
+  controller_nh.getParam("Kf", Kf_);
+
+  joint_ = effort_joint_interface->getHandle(joint_name_);
+
+  state_sub_ = controller_nh.subscribe("state", 1, &BuffController::update_current_state, this);
+  target_vel_pub_ = controller_nh.advertise<std_msgs::Float64>("target_vel", 10);
+  // load PID Controller using gains set on parameter server
+  joint_pid_controller_.init(ros::NodeHandle(controller_nh, "joint_pid"));
+
+  state_ = SMALL;
+
+  start_time_ = ros::Time::now();
+
+  return true;
+}
+
+void BuffController::update(const ros::Time& time, const ros::Duration& period)
+{
+  current_time_ = time;
+  period_ = period;
+
+  update_target_vel();
+  update_joint_state();
+
+  error_ = target_vel_ - current_velocity_;
+  if (feedForward_)
+  {
+    error_target_ = target_vel_ - last_target_vel_;
+    feedForward_out_ = error_target_ * Kf_;
+  }
+  else
+  {
+    feedForward_out_ = 0;
+  }
+  pid_out_ = joint_pid_controller_.computeCommand(error_, period_);
+  commanded_effort_ = pid_out_ + feedForward_out_;
+
+  joint_.setCommand(commanded_effort_);
+}
+
+void BuffController::update_joint_state()
+{
+  current_effort_ = joint_.getEffort();
+  current_velocity_ = joint_.getVelocity();
+  current_position_ = joint_.getPosition();
+}
+
+void BuffController::update_target_vel()
+{
+  static double a{}, b{}, w{}, lower_bound{}, upper_bound{}, t{};
+
+  if (state_changed_)
+  {
+    boost::random::mt19937 gen(static_cast<unsigned int>(std::time(nullptr)));
+
+    // 定义随机数分布范围
+    lower_bound = 0.780;
+    upper_bound = 1.045;
+    boost::random::uniform_real_distribution<> dis(lower_bound, upper_bound);
+    a = dis(gen);  // 生成一个随机小数
+
+    lower_bound = 1.884;
+    upper_bound = 2.000;
+    dis.param(boost::random::uniform_real_distribution<>::param_type(lower_bound, upper_bound));
+
+    w = dis(gen);  // 生成一个随机小数
+    b = 2.090 - a;
+    start_time_ = ros::Time::now();
+    state_changed_ = false;
+  }
+  last_target_vel_ = target_vel_;
+  if (state_ == SMALL)
+  {
+    target_vel_ = 10;
+  }
+  else if (state_ == BIG)
+  {
+    t = (current_time_ - start_time_).toSec();
+    target_vel_ = a * sin(w * t) + b;
+  }
+  std_msgs::Float64 target_vel_msg;
+  target_vel_msg.data = target_vel_;
+  target_vel_pub_.publish(target_vel_msg);
+}
+
+void BuffController::update_current_state(const std_msgs::Int32::ConstPtr& msg)
+{
+  if (msg->data != state_)
+    state_changed_ = true;
+  else
+    state_changed_ = false;
+  state_ = static_cast<BuffState>(msg->data);
+}
+
+PLUGINLIB_EXPORT_CLASS(buff_controller::BuffController, controller_interface::ControllerBase)
+}  // namespace buff_controller
